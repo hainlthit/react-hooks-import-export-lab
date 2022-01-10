6939d008cc2212870095335d7642623a0d1fb5ff
Hain Thit <hainlthit@gmail.com>
2022-01-10 10:48:44 -0500

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import About from "/home/hainthit/Development/code/phase2/react-hooks-import-export-lab/src/components/About.js";
+import Home from "/home/hainthit/Development/code/phase2/react-hooks-import-export-lab/src/components/Home.js";
+import NavBar from "/home/hainthit/Development/code/phase2/react-hooks-import-export-lab/src/components/NavBar.js";
 
 function App() {
   return (
